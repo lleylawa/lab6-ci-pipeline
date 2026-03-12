0cf6d7c9898164cbbcfe8dc9a20789cbe6ac78bb
lleylawa <lleylawa@gmail.com>
2026-03-13 01:27:55 +0600

    Add utils and tests

---
+++ src/utils.py
@@ -0,0 +1,5 @@
+def is_even(n: int) -> bool:
+    return n % 2 == 0
+
+def is_positive(n: int) -> bool:
+    return n > 0
---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from src.utils import is_even, is_positive
+
+def test_is_even():
+    assert is_even(2) is True
+    assert is_even(3) is False
+
+def test_is_positive():
+    assert is_positive(5) is True
+    assert is_positive(-1) is False
